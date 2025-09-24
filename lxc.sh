f6bdbcd54310d4bf945ed9713b06e0894d226248
Darth Vader <Chris.Geese@iCloud.com>
2025-09-24 14:50:10 -0400

    color fix #4

--- lxc.sh
+++ lxc.sh
@@ -2,14 +2,14 @@
 # A streamlined LXC creation script for Proxmox VE with colors and style
 
 # ---------- Colors ----------
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-YELLOW="\033[1;33m"
-BLUE="\033[0;34m"
-MAGENTA="\033[0;35m"
-CYAN="\033[0;36m"
-BOLD="\033[1m"
-RESET="\033[0m"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+BOLD='\033[1m'
+RESET='\033[0m'
 
 print_info()    { echo -e "${CYAN}[INFO]${RESET} $*"; }
 print_warn()    { echo -e "${YELLOW}[WARN]${RESET} $*"; }
