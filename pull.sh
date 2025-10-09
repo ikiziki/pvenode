742c941790db45f1aee4579c22f6f21c38843dd1
Kizik <84981131+ikiziki@users.noreply.github.com>
2025-10-09 12:17:56 -0400

    fix pull

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Always work from /usr/local/bin/pvenode
+# Always work from /usr/local/sbin/pvenode
 cd /usr/local/sbin/pvenode || exit 1
 
 # Color codes
@@ -12,8 +12,8 @@ RESET="\033[0m"
 echo -e "${YELLOW}Fetching all branches...${RESET}"
 git fetch --all
 
-echo -e "${YELLOW}Pulling latest changes from origin...${RESET}"
-git pull origin
+echo -e "${YELLOW}Resetting local changes to origin/main...${RESET}"
+git reset --hard origin/main
 
 echo -e "${GREEN}Setting executable permissions for scripts...${RESET}"
 chmod +x lxc.sh
@@ -26,4 +26,3 @@ chmod +x toggle.sh
 echo -e "${GREEN}  toggle.sh is now executable.${RESET}"
 
 echo -e "${GREEN}Done!${RESET}"
-
