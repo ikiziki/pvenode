ee6befda479b9a40bd8b593a17742c141d29552e
Kizik <84981131+ikiziki@users.noreply.github.com>
2025-10-09 12:22:47 -0400

    remove deploy script from pull

--- pull.sh
+++ pull.sh
@@ -20,8 +20,6 @@ chmod +x lxc.sh
 echo -e "${GREEN}  lxc.sh is now executable.${RESET}"
 chmod +x pull.sh
 echo -e "${GREEN}  pull.sh is now executable.${RESET}"
-chmod +x deploy-lxc.sh
-echo -e "${GREEN}  deploy-lxc.sh is now executable.${RESET}"
 chmod +x toggle.sh
 echo -e "${GREEN}  toggle.sh is now executable.${RESET}"
 
